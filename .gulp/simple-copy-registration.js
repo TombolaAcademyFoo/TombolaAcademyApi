8243d7826ab07f2800b7110824141e7182c70976
keith.barrow <keith.barrow@tombola.com>
2016-01-08 12:17:46 +0000

    Fixed problems due to sloppy merge...

--- .gulp/simple-copy-registration.js
+++ .gulp/simple-copy-registration.js
@@ -1,54 +1,57 @@
 (function () {
     'use strict';
 
-    var gulp = require('gulp'),
-        rimraf = require('gulp-rimraf'),
-        server = require('gulp-develop-server'),
-        watch = require('gulp-watch'),
-        simpleCopyGlobs = {
-            core: ['*.js', 'config.js', 'core-server/**/'],
-            api: 'api/*.js',
-            users: 'users/**'
-        },
-        createDestinationGlob = function (pattern) {
-            var buildRootPath = './.build/',
-                returnValue,
-                i;
+    var gulp = require('gulp');
+    var del = require('del');
+    var vinylPaths = require('vinyl-paths');
+    var server = require('gulp-develop-server');
+    var watch = require('gulp-watch');
 
-            if (Object.prototype.toString.call(pattern) === '[object Array]') {
-                returnValue = []
-                for (i = 0; i < pattern.length; i++) {
-                    returnValue.push(createDestinationGlob(pattern[i]));
-                }
+    var simpleCopyGlobs = {
+        core: ['*.js', 'config.js', 'core-server/**/'],
+        api: 'api/*.js',
+        users: 'users/**'
+    };
+
+    var createDestinationGlob = function (pattern) {
+        var buildRootPath = './.build/',
+            returnValue,
+            i;
+
+        if (Object.prototype.toString.call(pattern) === '[object Array]') {
+            returnValue = []
+            for (i = 0; i < pattern.length; i++) {
+                returnValue.push(createDestinationGlob(pattern[i]));
             }
-            else {
-                returnValue = buildRootPath + pattern;
-            }
-            return returnValue;
-        },
-        registerSimpleCopyTasks = function (name) {
-            var cleanTaskName = 'clean:' + name,
-                copyTaskName = 'copy:' + name,
-                watchTaskName = 'watch:' + name,
-                globPattern = simpleCopyGlobs[name];
+        }
+        else {
+            returnValue = buildRootPath + pattern;
+        }
+        return returnValue;
+    };
 
-            gulp.task(cleanTaskName, function () {
-                return gulp.src(createDestinationGlob(globPattern), {read: false})
-                    .pipe(rimraf());
-            });
+    var registerSimpleCopyTasks = function (name) {
+        var cleanTaskName = 'clean:' + name,
+            copyTaskName = 'copy:' + name,
+            watchTaskName = 'watch:' + name,
+            globPattern = simpleCopyGlobs[name];
 
-            gulp.task(copyTaskName, [cleanTaskName], function () {
-                return gulp.src(globPattern, { cwd: './app/', base:'./app'})
-                    .pipe(gulp.dest('./.build'));
-            });
+        gulp.task(cleanTaskName, function () {
+            return gulp.src(createDestinationGlob(globPattern), {read: false})
+                .pipe(vinylPaths(del));
+        });
 
-            gulp.task(watchTaskName, [copyTaskName], function () {
-                gulp.watch(globPattern, {base: './'}, [copyTaskName]);
-            });
-        };
+        gulp.task(copyTaskName, [cleanTaskName], function () {
+            return gulp.src(globPattern, {cwd: './app/', base: './app'})
+                .pipe(gulp.dest('./.build'));
+        });
+
+        gulp.task(watchTaskName, [copyTaskName], function () {
+            gulp.watch(globPattern, {base: './'}, [copyTaskName]);
+        });
+    };
 
     Object.keys(simpleCopyGlobs).forEach(function (name) {
-        console.log('Adding task ' + name);
         registerSimpleCopyTasks(name);
     });
 
